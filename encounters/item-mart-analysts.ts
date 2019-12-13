0824864a9cda3ecd0656fb50d8565bb12a6add58
Jim Kang <jimkang@gmail.com>
2019-12-13 17:48:20 -0500

    Reasonable-to-absorb layout

--- encounters/item-mart-analysts.ts
+++ encounters/item-mart-analysts.ts
@@ -5,8 +5,8 @@ export var itemMartAnalysts: Record<string, Beat> = {
     id: 'doorKnock',
     img: 'https://smidgeo.com/bots/media/slop-cube4.gif',
     imgAlt: 'A picture of two guys',
-    desc: `<p>Fred and Joe are at your door.</p>
-          <p>They describe themselves as Growth Opportunity Analysts for Item Mart. They are interested in your experiences with the Temple of Elemental Evil. They feel you have a unique perspective, given that you also have a background at Item Mart as a <em>[you hear paper rustling]</em> Service Associate I.</p>
+    desc: `<p>Fred and Joe are at the door of your inn room.</p>
+          <p>They describe themselves as Growth Opportunity Analysts for Item Mart. They are interested in your experiences with the Temple of Elemental Evil.
           <p>They mention that they want to pay you a 50 gp consulting fee.</p>`,
     question: 'What do you do?',
     playerOptions: [
@@ -33,8 +33,8 @@ export var itemMartAnalysts: Record<string, Beat> = {
         }
       },
       {
-        id: 'openDoor',
-        desc: 'Open the door.',
+        id: 'inviteIn',
+        desc: 'Invite them in.',
 
         next({ state }) {
           state.gp = 100;
@@ -50,6 +50,6 @@ export var itemMartAnalysts: Record<string, Beat> = {
   scouting: {
     id: 'scouting',
     desc:
-      '<p>Item Mart has heard that there is an underserved market in the Temple of Elemental Evil.</p>'
+      '<p>Item Mart has heard that there is a wealthy underserved market in the Temple of Elemental Evil.</p><p>They feel you have a unique perspective, given that you also have a background at Item Mart as a <em>[you hear paper rustling]</em> Service Associate I.</p>'
   }
 };
